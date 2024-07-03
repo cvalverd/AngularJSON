4452f9d324db6710200474444230d6f434955ed1
cvalverd <cvalverd.os@gmail.com>
2024-07-03 16:57:20 -0400

    dockerfile añadido

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ListaPersonasComponent } from './components/lista-personas/lista-personas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'lista-personas', pathMatch: 'full' },
+    {path: 'lista-personas', component: ListaPersonasComponent},
+    {path: '**', redirectTo: 'lista-personas'}
+];
